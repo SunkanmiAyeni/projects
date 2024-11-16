aa86a034b7e9c2a1e0ca2099445ffa5ebc4fbb21
SunkanmiAyeni <jumokeoayeni@gmail.com>
2024-11-16 14:35:22 +0100

    h/'w

---
+++ age calculator.py
@@ -0,0 +1,4 @@
+print ("How long have you lived")
+Name=input ("Name : ")
+print("What is your name?",(name))
+age=int(input(age:))
---
+++ square root.py
@@ -0,0 +1,4 @@
+import math
+user_input=int (input("Enter a number : "))
+res=math.isqrt(user_input)
+print("result:",res)
